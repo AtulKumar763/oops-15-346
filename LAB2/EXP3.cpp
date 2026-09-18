779f46f55866777d97e014b14f879151356ba1e7
AtulKumar763 <atulk22032007@gmail.com>
2026-09-18 10:23:55 +0530

    oops

---
+++ LAB2/EXP3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+// function1
+    void function1(float &salary){
+    salary = salary + (salary * 10/100);
+    }
+//function2
+    void function2(float *salary)
+{
+    *salary = *salary + (*salary * 10/100);
+}
+int main(){
+    float salary1 = 50000;
+    float salary2 = 50000;
+
+    function1(salary1);
+    function2(&salary2);
+
+    cout<<"function 1 : "<<salary1<<endl;
+    cout<<"function 2 : "<<salary2<<endl;
+
+return 0;
+}

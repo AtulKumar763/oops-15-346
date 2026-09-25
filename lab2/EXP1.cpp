17876975fd85c22eb346d67d5dd0f15c553dcd89
AtulKumar763 <atulk22032007@gmail.com>
2026-09-25 10:00:59 +0530

    folder

--- lab2/EXP1.cpp
+++ lab2/EXP1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
  
-    float salary = 100.0;
+    float salary = 101.0;
     float &newsalary = salary;
     newsalary = newsalary + (newsalary *  10/100);
     cout<<"salary : "<<salary<<endl;
